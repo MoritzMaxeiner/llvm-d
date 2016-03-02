1464c97c4d92846f89052e90a0d3eaa332ea3a6c
Alex 'Colonel Thirty Two' Parrill <initrd.gz@gmail.com>
2016-03-01 20:23:08 -0500

    Cleaned up llvm.util.templates
    - Removed commented code
    - Removed unneccessairy aliases in MixinMap
    - Removed the comment and copy about making a copy of the list in case of enum;
      the enum is instantiated once when its passed as an argument, not repeatedly
      through the function.

--- llvm/util/templates.d
+++ llvm/util/templates.d
@@ -7,7 +7,7 @@ private
 	import std.traits : isArray, isAssociativeArray, isCallable;
 }
 
-public string MixinMap(List,Function)(List l, Function f)
+public string MixinMap(List,Function)(List list, Function f)
 in
 {
 	assert(isArray!(List) || isAssociativeArray!(List));
@@ -17,26 +17,7 @@ body
 {
 	string code = "";
 
-	/+ Store the list (which may be coming in as
-		 + an enum (e.g. "enum string[] ...") in a
-		 + variable, so in case of an enum it won't
-		 + get simply textual-copied everywhere inside
-		 + the function - which would slow this down
-		 + significantly, so DO NOT REMOVE! +/
-	List list = l;
-
-	static if(isAssociativeArray!(List))
-	{
-		alias typeof(list.keys[0]) Index;
-		alias typeof(list.values[0]) Value;
-	}
-	else
-	{
-		alias size_t Index;
-		alias typeof(list[0]) Value;
-	}
-
-	foreach(Index index, Value value; l)
+	foreach(index, value; list)
 	{
 		code ~= f(index, value);
 	}
@@ -67,71 +48,3 @@ body
 					return "";
 				})[0..$-1] ~ " }";
 }
-
-/+public template MixinMap(alias list, alias f)
-{
-	const char[] MixinMap = ctfe_worker();
-
-	char[] ctfe_worker()
-	{
-		alias typeof(list) List;
-		/+ Store the list (which may be coming in as
-		 + an enum (e.g. "enum string[] ...") in a
-		 + variable, so in case of an enum it won't
-		 + get simply textual-copied everywhere inside
-		 + the function - which would slow this down
-		 + significantly, so DO NOT REMOVE! +/
-		List list = list;
-		char[] code = null;
-
-		/+ We need the list as an array +/
-		assert(isArray!(List) || isAssociativeArray!(List));
-
-		static if(isAssociativeArray!(List))
-		{
-			alias typeof(list.keys[0]) Index;
-			alias typeof(list.values[0]) Value;
-		}
-		else
-		{
-			alias size_t Index;
-			alias typeof(list[0]) Value;
-		}
-
-		foreach(Index index, Value value; list)
-		{
-			code ~= f(index, value);
-		}
-
-		return code;
-	}
-}
-
-public template MixinMap_VersionedEnum(
-	string enumName,
-	string enumType,
-	string enumVersion,
-	alias enumList)
-{
-	const char[] MixinMap_VersionedEnum = ctfe_worker();
-
-	char[] ctfe_worker()
-	{
-		return "enum"
-			~ (enumName !is null ? (" " ~ enumName) : "")
-			~ (enumType !is null ? (" : " ~ enumType) : "")
-			~ " { "
-			~ MixinMap!(
-				enumList,
-				function const(char)[] (string item, string[] change)
-				{
-					if((change is null) ||
-					   ((change[0] == "+") && (to!float(change[1]) <= to!float(enumVersion))) ||
-					   ((change[0] == "-") && (to!float(change[1]) > to!float(enumVersion))))
-					{
-						return item ~ ",";
-					}
-					return null;
-				})[0..$-1] ~ " }";
-	}
-}+/
