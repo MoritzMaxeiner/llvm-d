ebfa6d57f9f2f5da495515dfdcad5e513aab7a16
Moritz Maxeiner <mm@ucw.sh>
2020-06-12 00:43:32 +0200

    Add missing minor versions

--- source/llvm/config.d
+++ source/llvm/config.d
@@ -7,10 +7,17 @@ import std.algorithm.searching : canFind;
 
 /// LLVM Versions that llvm-d supports
 immutable LLVM_Versions = [
+	[8,0,1],
 	[8,0,0],
+	[7,1,0],
+	[7,0,1],
 	[7,0,0],
+	[6,0,1],
 	[6,0,0],
+	[5,0,2],
+	[5,0,1],
 	[5,0,0],
+	[4,0,1],
 	[4,0,0],
 	[3,9,1],
 	[3,9,0],
